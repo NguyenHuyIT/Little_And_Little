94d974e6bc25f073a267c419d59eab19dcae3b93
NguyenHuyIT <nguyenhuy.pt1505@gmail.com>
2022-05-18 15:54:47 +0700

    add_laravel_and_cut_layout

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+Route::get('/payment', function () {
+    return view('payment');
+})->name('payment');
+Route::get('/event', function () {
+    return view('event');
+})->name('event');
+Route::get('/event-detail', function () {
+    return view('event-detail');
+})->name('event-detail');
+Route::get('/payment-success', function () {
+    return view('payment-success');
+})->name('payment-success');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
